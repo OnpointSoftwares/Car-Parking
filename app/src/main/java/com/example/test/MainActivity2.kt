27a88bf8580a690d2f3332f1fb440ffecbc72e1d
OnpointSoftwares <vincentbettoh@gmail.com>
2023-07-03 16:17:43 -0400

    Changes

--- app/src/main/java/com/example/test/MainActivity2.kt
+++ app/src/main/java/com/example/test/MainActivity2.kt
@@ -1,9 +1,17 @@
 package com.example.test
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.getValue
 
 class MainActivity2 : AppCompatActivity() {
     lateinit var courseRV: RecyclerView
@@ -30,7 +38,24 @@ class MainActivity2 : AppCompatActivity() {
         // on below line we are setting
         // adapter to our recycler view.
         courseRV.adapter = courseRVAdapter
+        // Read from the database
+        val database:FirebaseDatabase=FirebaseDatabase.getInstance()
+        val myRef=database.reference.child("parkings")
+        myRef.addValueEventListener(object: ValueEventListener {
 
+            override fun onDataChange(snapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                val value = snapshot.getValue<String>()
+                Log.d(TAG, "Value is: $value")
+            Toast.makeText(this@MainActivity2,value,Toast.LENGTH_LONG).show()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.w(TAG, "Failed to read value.", error.toException())
+            }
+
+        })
         // on below line we are adding data to our list
         courseList.add(parking_Model("Android Development","Available"))
         courseList.add(parking_Model("C++ Development", "available"))
