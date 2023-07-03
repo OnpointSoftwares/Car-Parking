513da2d30ac11c75d09b4fb7fedf361a3215cdcc
OnpointSoftwares <vincentbettoh@gmail.com>
2023-07-03 15:45:53 -0400

    Changes

---
+++ app/src/main/java/com/example/test/parking_Model.kt
@@ -0,0 +1,9 @@
+package com.example.test
+
+data class parking_Model(
+    // on below line we are creating a
+    // two variable one for course name
+    // and other for course image.
+    var ParkingNumber: String,
+    var ParkingAvailability: Int
+)
