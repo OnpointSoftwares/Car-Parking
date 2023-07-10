79316f5e08b46e8e326a46e7c98f3accc56d7c9b
OnpointSoftwares <vincentbettoh@gmail.com>
2023-07-10 11:50:55 -0400

    Changes

--- app/src/main/java/com/example/test/MainActivity3.kt
+++ app/src/main/java/com/example/test/MainActivity3.kt
@@ -2,10 +2,40 @@ package com.example.test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity3 : AppCompatActivity() {
+
+    private lateinit var pager: ViewPager // creating object of ViewPager
+    private lateinit var tab: TabLayout // creating object of TabLayout
+    private lateinit var bar: Toolbar // creating object of ToolBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
+
+        // set the references of the declared objects above
+        pager = findViewById(R.id.viewPager)
+        tab = findViewById(R.id.tabs)
+        bar = findViewById(R.id.toolbar)
+
+        // To make our toolbar show the application
+        // we need to give it to the ActionBar
+        setSupportActionBar(bar)
+
+        // Initializing the ViewPagerAdapter
+        val adapter = ViewPagerAdapter(supportFragmentManager)
+
+        // add fragment to the list
+        adapter.addFragment(ParkingSpacesFragment(), "GeeksForGeeks")
+        adapter.addFragment(BookingsFragment(), "Code Chef")
+
+        // Adding the Adapter to the ViewPager
+        pager.adapter = adapter
+
+        // bind the viewPager with the TabLayout.
+        tab.setupWithViewPager(pager)
     }
 }
